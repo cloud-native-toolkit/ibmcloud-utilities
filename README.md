a231fa4a01c5c9b5c262391c5413d46394251672
balasgit <bala@us.ibm.com>
2022-07-29 15:52:37 -0500

    initial version

--- README.md
+++ README.md
@@ -1,1 +1,138 @@
-# ibmcloud-utilities
+# ibmcloud-nuke
+
+Remove all resources from an IBM Cloud account.
+
+## Install the IBM Cloud CLI & plugins
+
+In order to use this script you must have installed the IBM Cloud CLI. If you need to do that run the following:
+```
+curl -fsSL https://clis.cloud.ibm.com/install/linux | sh
+```
+
+Once IBM Cloud CLI installed,  install several plugins for the script to execute successfully.
+install them run the following commands:
+
+```
+ibmcloud plugin install -f code-engine
+```
+
+```
+ibmcloud plugin install -f container-registry
+```
+
+```
+ibmcloud plugin install -f container-service
+```
+
+```
+ibmcloud plugin install -f cloud-functions
+```
+
+```
+ibmcloud plugin install -f infrastructure-service
+```
+
+```
+ibmcloud plugin install -f schematics
+```
+
+```
+ibmcloud plugin install cloud-object-storage
+```
+
+```
+ibmcloud plugin install vpc-infrastructure
+```
+
+```
+ibmcloud target -g <Resource-Group>
+```
+
+## Caution!
+
+Be aware that *IBM Cloud Nuke* is a very destructive tool, be very careful while using it. Otherwise you might delete production data. **Do NOT run this application on an IBM Cloud account where you cannot afford to lose all resources.**
+
+Some safety precautions have been put in place.
+
+1. By default *IBM Cloud Nuke* only lists all nukeable resources. You need to add `-n` flag to actually delete resources.
+1. A config file can be used to specify names and IDs of resources to skip over.
+
+The project itself is just a shell script that is required be run by an authenticated IBM Cloud user. The shell script will find and delete the following resources:
+
+* Kubernetes and OpenShift clusters
+* Container Registry namespaces
+* Applications (Cloud Foundry or Stater Kits)
+* Services (like Cloudant, Watson services, Object Storage (and their underlying buckets), etc)
+* Classic Baremetal servers
+* Classic Virtual servers
+* Code Engine projects (and their underlying jobs)
+* Cloud Functions Namespaces (and their underlying actions)
+* API keys (excluding ones requires for managing Kubernetes clusters)
+* Satellite locations
+* Gen2 VPCs
+* VPC Subnet
+* VPC Loadbalancer
+  
+
+## CLI options
+
+```bash
+main.sh [-n] [-c <path-to-config-file>]
+```
+
+* `-n`: Specify "no dry run". Resources will be deleted.
+* `-c`: Specify a config file for IDs/names of resources to be saved from deletion. The tool will automatically look for a file called `ibmcloud-nuke` in the local directory.
+
+## Using the CLI
+
+1. Clone the repo.
+
+   ```bash
+   git clone
+   ```
+
+2. Log into IBM Cloud.
+
+   ```bash
+   ibmcloud login --sso
+   ```
+3. Switch to Resource Group where you want to list and delete resources
+
+   ```bash
+   ibmcloud target -g <resource-group>
+   ```
+4. Switch to region where you want to list and delete resources
+
+   ```bash
+   ibmcloud target -r <region us-south,eu-gb>
+   ```
+
+4. Run the CLI.
+
+   Perform a dry run:
+
+   ```bash
+   ./ibmcloud-delete.sh
+   ```
+
+   Delete all resources (will skip over any resorces found in `.ibmcloud-nuke`):
+
+   ```bash
+   ./ibmcloud-delete.sh -n
+   ```
+
+   Delete all resources but skip resources list in `myfile.txt`:
+
+   ```bash
+   ./ibmcloud-delete.sh -c myfile.txt
+   ```
+
+## TODO
+
+1. The project needs support for deleting the following types of resources:
+
+   * [Schematics Workspaces](https://cloud.ibm.com/docs/schematics?topic=schematics-schematics-cli-reference#schematics-workspace-delete)
+
+## Resources
+
+1. [IBM Cloud CLI](https://cloud.ibm.com/docs/cli?topic=cli-getting-started)
---
+++ ibmcloud-delete.sh
@@ -0,0 +1,279 @@
+#!/bin/bash
+
+# This script is **destructive** and will delete a lot of IBM
+# Cloud resources. Use with caution!
+
+# Lastly, you must be authenticated and have targetted
+# a resource group. To do so, run the following:
+
+# ibmcloud login --sso
+# ibmcloud target -g <resource-group>
+
+
+usage() { echo "Usage: $0 [-n] [-c <config-file>]" 1>&2; exit 1; }
+
+NO_DRY_RUN=''
+CONFIG_FILE=''
+while getopts "nc:" o; do
+    case "${o}" in
+        n)
+            n=${OPTARG}
+            NO_DRY_RUN=1
+            ;;
+        c)
+            c=${OPTARG}
+            CONFIG_FILE=$c
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+if [ "$CONFIG_FILE" ]; then
+    echo "Attempting to use config file at location: $CONFIG_FILE"
+    # Ensure config file exists
+    if ! test -f ${CONFIG_FILE};
+    then
+        echo "specified config file ${CONFIG_FILE} not found"
+        exit 1
+    fi
+else
+    CONFIG_FILE='.ibmcloud-nuke'
+    echo "Attempting to use config file at default location: $CONFIG_FILE"
+fi
+
+if [ "${NO_DRY_RUN}" ]; then
+    echo "The (-n) flag was found. Will begin to delete all resources."
+    NO_DRY_RUN=1
+else
+    echo "No (-n) flag found. Will NOT delete any resources."
+    NO_DRY_RUN=0
+fi
+
+#echo "================================="
+echo "NO_DRY_RUN = ${NO_DRY_RUN}"
+echo "CONFIG_FILE = ${CONFIG_FILE}"
+#echo "================================="
+
+# Usage: get_or_create_user <name_or_id>
+function check_config_file() {
+    if test -f ${CONFIG_FILE} && grep -Fxq "${1}" ${CONFIG_FILE}
+    then
+        echo "skipping ${name} as it exists in ${CONFIG_FILE}"
+        continue
+    fi
+}
+
+# Resource Group retrieve all under Account
+#echo "================================="
+echo "  "
+echo "Retrieving all resource groups under account: "
+#echo "  "
+#echo "================================="
+ibmcloud resource groups | tail -n+4 | while read -r name rest_of_cmd; do
+    echo "- ${name}"
+    check_config_file "${name}"
+
+    if [ "${NO_DRY_RUN}" == 1 ]; then
+        ibmcloud resource group-delete "${name}" -f
+    fi
+done
+
+# clusters
+#echo "================================="
+echo "  "
+echo "OpenShift Clusters: "
+#echo "================================="
+ibmcloud ks clusters -q | tail -n+2 | while read -r name rest_of_cmd ; do
+    echo "- ${name}"
+    check_config_file "${name}"
+
+    if [ "${NO_DRY_RUN}" == 1 ]; then
+        ibmcloud ks cluster rm -f -c ${name}
+    fi
+
+done
+
+# namespaces
+#echo "================================="
+echo "  "
+echo "Container Registry (namespaces): "
+#echo "================================="
+ibmcloud cr namespaces | tail -n+4 | while read -r name rest_of_cmd ; do
+    echo "- ${name}"
+    check_config_file "${name}"
+
+    if [ "${NO_DRY_RUN}" == 1 ]; then
+        ibmcloud cr namespace-rm -f ${name}
+    fi
+done
+
+# apps
+#echo "================================="
+echo "  "
+echo "Applications: "
+#echo "================================="
+ibmcloud dev list | tail -n+8 | while read -r name rest_of_cmd ; do
+    echo "- ${name}"
+    check_config_file "${name}"
+
+    if [ "${NO_DRY_RUN}" == 1 ]; then
+        ibmcloud dev delete -f ${name}
+    fi
+done
+
+# services - use awk here as the service name can include spaces
+#echo "================================="
+echo "  "
+echo "Services: "
+#echo "================================="
+ibmcloud resource service-instances | tail -n+4 | awk -F '  +' '{print $1}' | while read -r name; do
+    echo "- ${name}"
+    check_config_file "${name}"
+
+    if [ "${NO_DRY_RUN}" == 1 ]; then
+        ibmcloud resource service-instance-delete -f --recursive "${name}"
+    fi
+done
+
+# baremetal
+#echo "================================="
+echo "  "
+echo "Classic Baremetal: "
+#echo "================================="
+ibmcloud sl hardware list | grep -v 'kube-' | tail -n+2 | while read -r id rest_of_cmd ; do
+    echo "${id}"
+    check_config_file "${id}"
+
+    if [ "${NO_DRY_RUN}" == 1 ]; then
+        ibmcloud sl hardware cancel -f ${id}
+    fi
+done
+
+# VMs
+#echo "================================="
+echo "  "
+echo "Classic VMs: "
+#echo "================================="
+ibmcloud sl vs list | grep -v 'kube-' | tail -n+2 | while read -r id rest_of_cmd ; do
+    echo "- ${id}"
+    check_config_file "${id}"
+
+    if [ "${NO_DRY_RUN}" == 1 ]; then
+        ibmcloud sl vs cancel -f ${id}
+    fi
+done
+
+# code engine
+#echo "================================="
+echo "  "
+echo "Code Engine (Projects):"
+#echo "================================="
+ibmcloud ce project list | tail -n+5 | while read -r name rest_of_cmd ; do
+    echo "- ${name}"
+    check_config_file "${name}"
+
+    if [ "${NO_DRY_RUN}" == 1 ]; then
+        ibmcloud ce project delete -f --name ${name}
+    fi
+done
+
+# functions - use awk because namespaces can have spaces
+#echo "================================="
+echo "  "
+echo "Cloud Functions (namespaces): "
+#echo "================================="
+ibmcloud fn namespace list | tail -n+3 | awk -F '  +' '{print $1}' | while read -r name; do
+    echo "- ${name}"
+    check_config_file "${name}"
+
+    if [ "${NO_DRY_RUN}" == 1 ]; then
+        ibmcloud fn namespace delete "${name}"
+    fi
+done
+
+# satellite locations (cannot contain a space)
+#echo "================================="
+echo "  "
+echo "Satellite locations: "
+#echo "================================="
+ibmcloud sat location ls | tail -n+4 | while read -r name rest_of_cmd; do
+    echo "- ${name}"
+    check_config_file "${name}"
+
+    if [ "${NO_DRY_RUN}" == 1 ]; then
+        ibmcloud sat location rm --location "${name}" -f
+    fi
+done
+
+# Virtual Private Clouds (VPCs) (cannot contain a space)
+#echo "================================="
+echo "  "
+echo "Virtual Private Clouds: "
+#echo "================================="
+ibmcloud is vpcs | tail -n+3 | while read -r id rest_of_cmd; do
+    echo "- ${id}"
+    check_config_file "${id}"
+
+    if [ "${NO_DRY_RUN}" == 1 ]; then
+        ibmcloud is subnetd "${id}" -f
+    fi
+done
+
+# Virtual Private Clouds (VPCs) - Subnets (cannot contain a space)
+#echo "================================="
+echo "  "
+echo "Virtual Private Clouds - Subnets: "
+#echo "================================="
+ibmcloud is subnets | tail -n+3 | while read -r id rest_of_cmd; do
+    echo "- ${id}"
+    check_config_file "${id}"
+
+    if [ "${NO_DRY_RUN}" == 1 ]; then
+        ibmcloud is subnetd "${id}" -f
+    fi
+done
+
+# Virtual Private Clouds (VPCs) - LB (cannot contain a space)
+#echo "================================="
+echo "  "
+echo "Virtual Private Clouds - Load Balancer: "
+#echo "================================="
+ibmcloud is lbs | tail -n+3 | while read -r id rest_of_cmd; do
+    echo "- ${id}"
+    check_config_file "${id}"
+
+    if [ "${NO_DRY_RUN}" == 1 ]; then
+        ibmcloud is lbd "${id}" -f
+    fi
+done
+
+# Virtual Private Clouds (VPCs) - Regions (cannot contain a space)
+#echo "================================="
+echo "  "
+echo "Virtual Private Clouds - Regions: "
+#echo "================================="
+ibmcloud is regions | tail -n+3 | while read -r id rest_of_cmd; do
+    echo "-> ${id}"
+    check_config_file "${id}"
+
+    if [ "${NO_DRY_RUN}" == 1 ]; then
+        ibmcloud is region "${id}" -f
+    fi
+done
+
+# api keys - skip the keys that say "Do not delete"
+#echo "================================="
+echo "  "
+echo "API Keys: "
+#echo "================================="
+ibmcloud iam api-keys | tail -n+4 | grep -v 'Do not delete' | while read -r name rest_of_cmd; do
+    echo "- ${name}"
+    check_config_file "${name}"
+
+    if [ "${NO_DRY_RUN}" == 1 ]; then
+        ibmcloud iam api-key-delete ${name}
+    fi
+done
+
